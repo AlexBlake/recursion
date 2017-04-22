25b517d945623efdb1f08ca6d6db51e2ae407566
Alex Blake <cr3ch4@gmail.com>
2017-04-22 22:18:01 +0200

    asd

---
+++ Assets/Scripts/PlayerInteractionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionManager : MonoBehaviour {
+
+	public bool actionAvailable = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			
+		}
+
+
+
+	}
+		
+	void OnTriggerEnter2D (Collider2D other) {
+		actionAvailable = true;
+		//PlayerPrefs.GetInt ("isTalking", 1);
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		actionAvailable = false;
+		//PlayerPrefs.GetInt ("isTalking", 1);
+	}
+}
