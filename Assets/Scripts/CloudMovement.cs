60ec4c17b8da321496bcab82f09ed4e3c266cd34
flapjackstackpack <atomic101@rocketmail.com>
2017-04-22 23:13:55 +0200

    MeClouds XDDD

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour {
+
+	private float speed = 1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		gameObject.transform.position = new Vector3(gameObject.transform.position.x - (speed * Time.deltaTime), gameObject.transform.position.y, gameObject.transform.position.z);
+	}
+}
