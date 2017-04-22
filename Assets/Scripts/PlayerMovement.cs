04495555a423ee23afad43bff5dfc9dcc7089986
Alex Blake <cr3ch4@gmail.com>
2017-04-22 17:47:30 +0200

    Basic PLayer Movement
    Added basic player moement and temp model to replace with sprites.
    
    Ground collider to move with player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Player frame
+	public Rigidbody playerBody;
+	void Start() {
+		playerBody = GetComponent<Rigidbody>();
+	}
+
+	// Movement Variables
+	public float mvtMaxSpeed = 6.0f;
+	public float mvtDir;
+
+
+	// Use this for initialization
+	void FixedUpdate () {
+		// Move the player
+		playerBody.velocity= new Vector2(mvtDir * mvtMaxSpeed, playerBody.velocity.y);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Get Horizontal Direction
+		mvtDir = Input.GetAxis("Horizontal");
+	
+	}
+}
